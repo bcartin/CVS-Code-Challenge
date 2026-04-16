5969aa7c3644b7e2b66b46653e92d0c23e4af672
Bernal Cartin <bcartin@gmail.com>
2026-04-16 08:37:16 -0400

    Initial Commit

---
+++ CVS-Code-Challenge/CVS_Code_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVS_Code_ChallengeApp.swift
+//  CVS-Code-Challenge
+//
+//  Created by Bernie Cartin on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct CVS_Code_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
